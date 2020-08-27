eddc643169a587d89db7da3d1d700e8433f98b3b
Riyad0821 <mdriyad0821@gmail.com>
2020-08-27 14:27:50 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit Done <code>src/App.js</code> and save to reload.
-        </p>
+        <p>I am a React Person</p>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
       </header>
     </div>
   );
+}
+function Person(){
+  return (
+  <div style={{border: '2px solid blue', margin:'10px', padding:'5px'}}>
+      <h1>Name: Fazlay Rabby</h1>
+      <h3>Hero of the year</h3>
+  </div>
+  )
 }
 
 export default App;
